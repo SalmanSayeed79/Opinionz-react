0263a97c3d4a29455145d4db358ff6b92d45f1df
SalmanSayeed79 <66307068+SalmanSayeed79@users.noreply.github.com>
2020-07-25 19:28:05 +0600

    first commit (build folder)

---
+++ precache-manifest.40c40a34cd69ad096b35226a193e3f32.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "272bdbe6fd0c586fa13d4f18f4a8d756",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8d32cb1dc1b996b54ad0",
+    "url": "/static/css/main.7559ff65.chunk.css"
+  },
+  {
+    "revision": "a2bddc267117d45098bf",
+    "url": "/static/js/2.94d33db3.chunk.js"
+  },
+  {
+    "revision": "ad951958ed7766763ed3e6c72917393c",
+    "url": "/static/js/2.94d33db3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d32cb1dc1b996b54ad0",
+    "url": "/static/js/main.04aff24d.chunk.js"
+  },
+  {
+    "revision": "3268121bdaee7f3d4556",
+    "url": "/static/js/runtime-main.94d66e60.js"
+  }
+]);
